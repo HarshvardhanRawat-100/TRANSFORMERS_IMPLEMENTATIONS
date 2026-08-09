8b7c6e93feebbe823861b871d11e6a4233eadeee
HarshvardhanRawat-100 <harshvardhanrawat.102@gmail.com>
2026-08-10 01:31:58 +0530

    DOING DYNAMIC PROMPT

--- prompt/chat_prompt_dynamic_templ.py
+++ prompt/chat_prompt_dynamic_templ.py
@@ -1,3 +1,25 @@
-from langchain_core.messages import SystemMessage , HumanMessage ,AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 import os 
+
+load_dotenv()
+
+chat_template = ChatPromptTemplate([
+    ('system' , 'you are a helpfule {domain} expert') ,
+    ('human', 'explain in simple term what is {topics}')
+])
+
+prompt = chat_template.invoke({'domain' : 'cricket' , 'topics' : 'spin'})
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-1.5B-Instruct",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN"),
+    provider="featherless-ai"
+) 
+
+model = ChatHuggingFace(llm = llm)
+
+response = model.invoke(prompt)
+print(response.content)
