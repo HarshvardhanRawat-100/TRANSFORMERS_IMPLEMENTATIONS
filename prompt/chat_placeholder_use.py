f3023d28d256ca3bc90d413b58059a26f370caa5
HarshvardhanRawat-100 <harshvardhanrawat.102@gmail.com>
2026-08-10 02:24:23 +0530

    WORKING ON PLACEHOLDER

--- prompt/chat_placeholder_use.py
+++ prompt/chat_placeholder_use.py
@@ -1,19 +1,12 @@
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
-from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 from dotenv import load_dotenv
 
-load_dotenv
+load_dotenv()
 
-llm = HuggingFaceEndpoint(
-    repo_id="Qwen/Qwen2.5-1.5B-Instruct",
-    task="text-generation",
-    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN"),
-    provider="featherless-ai"
-) 
-
-model = ChatHuggingFace(llm = llm)
+model = ChatGoogleGenerativeAI(model = "gemini-3.5-flash",temperature=0.7)
 
 # Prompt Template
 chat_template = ChatPromptTemplate([
@@ -43,3 +36,32 @@ def save_message(role, content):
 
 # Load old chat
 load_history()
+
+# ===== CHAT LOOP =====
+print("🤖 Chatbot started (type 'exit' to stop)\n")
+
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        break
+
+    # Add user message to memory
+    chat_history.append(HumanMessage(content=user_input))
+    save_message("Human", user_input)
+
+    # Create prompt with history
+    prompt = chat_template.invoke({
+        'chat_history': chat_history,
+        'query': user_input
+    })
+
+    # Get response from LLM
+    response = model.invoke(prompt)
+
+    ai_reply = response.text
+    print("AI:", ai_reply)
+
+    # Save AI response
+    chat_history.append(AIMessage(content=ai_reply))
+    save_message("AI", ai_reply)
