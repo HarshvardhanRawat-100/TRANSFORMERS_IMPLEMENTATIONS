3298b9259c07dbf9068d22fa544adda34a9e3bb9
HarshvardhanRawat-100 <harshvardhanrawat.102@gmail.com>
2026-08-13 00:44:50 +0530

    CONDTIONAL CHAIN

---
+++ langchain_models/Chains/condtional_chains.py
@@ -0,0 +1,13 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.schema.runnable import RunnableParallel, RunnableBranch, RunnableLambda
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import Literal
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model = "gemini-3.5-flash",temperature=0.7 , google_api_key=os.getenv("GEMINI_API_KEY_2"))
+
