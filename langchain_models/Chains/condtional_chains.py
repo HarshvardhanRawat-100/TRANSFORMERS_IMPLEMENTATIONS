b04926e7c2c0fe10a25b41ebb0619154c6dd1c4c
HarshvardhanRawat-100 <harshvardhanrawat.102@gmail.com>
2026-08-13 00:59:31 +0530

    fixing code and CHECKING WORKFLOW THROUGH GRANDALF

--- langchain_models/Chains/condtional_chains.py
+++ langchain_models/Chains/condtional_chains.py
@@ -2,10 +2,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain.schema.runnable import RunnableParallel, RunnableBranch, RunnableLambda
+from langchain_core.runnables import RunnableParallel, RunnableBranch, RunnableLambda
 from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 from typing import Literal
+import os
 
 load_dotenv()
 
@@ -28,13 +29,23 @@ prompt1 = PromptTemplate(
 classifier_chain = prompt1 | model | parser2
 
 prompt2 = PromptTemplate(
-    template='Write an appropriate response to this positive feedback \n {feedback}',
-    input_variables=['feedback']
+    template="""Write ONE short and polite response to this positive feedback.
+Do not give multiple options.
+Do not explain your answer.
+Return only the response.
+
+Feedback: {feedback}""",
+    input_variables=["feedback"]
 )
 
 prompt3 = PromptTemplate(
-    template='Write an appropriate response to this negative feedback \n {feedback}',
-    input_variables=['feedback']
+    template="""Write ONE short and professional response to this negative feedback.
+Do not give multiple options.
+Do not explain your answer.
+Return only the response.
+
+Feedback: {feedback}""",
+    input_variables=["feedback"]
 )
 
 branch_chain = RunnableBranch(
@@ -44,3 +55,5 @@ branch_chain = RunnableBranch(
 
 chain = classifier_chain | branch_chain
 print(chain.invoke({'feedback': 'This is a beautiful phone'}))
+
+chain.get_graph().print_ascii()
