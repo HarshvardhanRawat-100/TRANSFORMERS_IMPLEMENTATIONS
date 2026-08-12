b8cf2f1326f5855d494dcd5e8385e5c1dd986e3d
HarshvardhanRawat-100 <harshvardhanrawat.102@gmail.com>
2026-08-13 00:53:54 +0530

    WARMING UP HANDS ON CONTIONAL CHAIN CODE

--- langchain_models/Chains/condtional_chains.py
+++ langchain_models/Chains/condtional_chains.py
@@ -11,3 +11,36 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(model = "gemini-3.5-flash",temperature=0.7 , google_api_key=os.getenv("GEMINI_API_KEY_2"))
 
+parser = StrOutputParser()
+
+class Feedback(BaseModel):
+
+    sentiment: Literal['positive', 'negative'] = Field(description='Give the sentiment of the feedback')
+
+parser2 = PydanticOutputParser(pydantic_object=Feedback)
+
+prompt1 = PromptTemplate(
+    template='Classify the sentiment of the following feedback text into postive or negative \n {feedback} \n {format_instruction}',
+    input_variables=['feedback'],
+    partial_variables={'format_instruction':parser2.get_format_instructions()}
+)
+
+classifier_chain = prompt1 | model | parser2
+
+prompt2 = PromptTemplate(
+    template='Write an appropriate response to this positive feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template='Write an appropriate response to this negative feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+branch_chain = RunnableBranch(
+    (lambda x:x.sentiment == 'positive', prompt2 | model | parser),
+    (lambda x:x.sentiment == 'negative', prompt3 | model | parser),
+    RunnableLambda(lambda x: "could not find sentiment") )
+
+chain = classifier_chain | branch_chain
+print(chain.invoke({'feedback': 'This is a beautiful phone'}))
