953f94406c4b67122a606a70d90916f5e1503bb5
HarshvardhanRawat-100 <harshvardhanrawat.102@gmail.com>
2026-08-15 17:28:15 +0530

    RUNNABLE LAMBDA

---
+++ langchain_models/Runnables/runnable_lambda.py
@@ -0,0 +1,32 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+import os
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence , RunnableParallel , RunnablePassthrough
+
+load_dotenv()
+parser = StrOutputParser()
+# Define the model
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-1.5B-Instruct",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN"),
+    provider="featherless-ai"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+def word_count(text):
+    return len(text.split())
+
+prompt = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+joke_gen_chain = RunnableSequence(prompt, model, parser)
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'word_count': RunnableLambda(word_count)
+})
