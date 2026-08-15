4043bcbd20c21b32d0477947519921fa1afb595b
HarshvardhanRawat-100 <harshvardhanrawat.102@gmail.com>
2026-08-15 17:30:48 +0530

    RUNNABLE LAMBDA AND ITS WORKFLOW

--- langchain_models/Runnables/runnable_lambda.py
+++ langchain_models/Runnables/runnable_lambda.py
@@ -3,7 +3,7 @@ from langchain_core.prompts import PromptTemplate
 import os
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain_core.runnables import RunnableSequence , RunnableParallel , RunnablePassthrough
+from langchain_core.runnables import RunnableSequence , RunnableParallel , RunnablePassthrough , RunnableLambda
 
 load_dotenv()
 parser = StrOutputParser()
@@ -30,3 +30,13 @@ parallel_chain = RunnableParallel({
     'joke': RunnablePassthrough(),
     'word_count': RunnableLambda(word_count)
 })
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+result = final_chain.invoke({'topic':'AI'})
+
+final_result = """{} \n word count - {}""".format(result['joke'], result['word_count'])
+
+print(final_result)
+final_chain.get_graph().print_ascii()
+
