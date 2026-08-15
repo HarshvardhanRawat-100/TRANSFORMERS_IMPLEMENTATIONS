d521fa19fc4d1d29227ecd2143af84aa8c4f1a02
HarshvardhanRawat-100 <harshvardhanrawat.102@gmail.com>
2026-08-15 16:14:24 +0530

    RUNNABLE

---
+++ langchain_models/Runnables/runnable_seq.py
@@ -0,0 +1,29 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+import os
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence
+
+load_dotenv()
+parser = StrOutputParser()
+# Define the model
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-1.5B-Instruct",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN"),
+    provider="featherless-ai"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+prompt1 = PromptTemplate(
+    template = "Write joke about this {topic}" ,
+    input_variables = ['topic']
+)
+prompt2 = PromptTemplate(
+    template = "Write summary of this {text}" ,
+    input_variables = ['text']
+)
+chain = RunnableSequence(prompt1,model,parser,prompt2,model,parser)
+print(chain.invoke({'topic' : 'AI'}))
