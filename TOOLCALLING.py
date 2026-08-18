97b909e05a484f2bb5143530e4efd22a57433601
HarshvardhanRawat-100 <harshvardhanrawat.102@gmail.com>
2026-08-18 14:49:42 +0530

    TOOL CALL , TOOL BINDING , TOOL EXECUTION

--- TOOLCALLING.py
+++ TOOLCALLING.py
@@ -5,3 +5,28 @@ from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage
 load_dotenv()
 model = ChatGoogleGenerativeAI(model = "gemini-3.5-flash",temperature=0.7 , google_api_key=os.getenv("GOOGLE_API_KEY_2"))
+
+# tool create
+
+@tool
+def multiply(a: int, b: int) -> int:
+  """Given 2 numbers a and b this tool returns their product"""
+  return a * b
+ 
+# tool binding 
+llm_with_tools = model.bind_tools([multiply]) 
+ 
+query = HumanMessage('multiply 3 with 1000')
+
+message = [query]
+
+# tool calling 
+
+result = llm_with_tools.invoke(message)
+
+message.append(result)
+
+# tool execution
+
+tool_result = multiply.invoke(result.tool_calls[0])
+message.append(tool_result)
