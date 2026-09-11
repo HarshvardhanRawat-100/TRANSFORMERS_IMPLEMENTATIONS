a7810d6571b3f4256ea3eb9154c70505a9c37419
HarshvardhanRawat-100 <harshvardhanrawat.102@gmail.com>
2026-09-12 02:18:19 +0530

    SIMILIRITY SEARCH RETRIEVER

---
+++ ADVANCE_RAG/Retriever/similiarity_search.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+
+load_dotenv()
